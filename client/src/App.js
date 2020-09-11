27bd1d7394593f2150e01e8337e0504bd3d97e9c
jerry <jerrylai19990120@gmail.com>
2020-09-11 16:34:00 -0400

    login and sigup page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateRoom from "./components/Home";
 import Room from "./components/Room";
+import Login from './components/login';
+import SignUp from './components/signup';
 import './App.css';
 
 
@@ -10,7 +12,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={CreateRoom} />
+          <Route path="/" exact component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/home" exact component={CreateRoom} />
           <Route path="/room/:roomID" component={Room} />
         </Switch>
       </BrowserRouter>
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 
 class Login extends React.Component{
 
     render(){
         return(
-            <div>
-
+            <div style={{height:'100vh', width:'100vw', backgroundColor:'#47C5E3'}}>
+                <div>
+                    <h1 style={{}}>Welcome to Vivideo!</h1>
+                    <h2>Join our diverse users community.</h2>
+                </div>
+                
+                <form>
+                    <TextField id="standard-basic" label="Username" /><br/>
+                    <TextField
+                        id="standard-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                    /><br/>
+                    <Button variant="contained" color="primary">
+                        Log in
+                    </Button><br/>
+                    <span>or <a href="/signup" style={{textDecoration:'none', color:'white'}}>Sign up</a> to join our community.</span>
+                </form>
             </div>
         )
     }
