c14b232abeb823463b256512241b4e5f234cc7af
jerry <jerrylai19990120@gmail.com>
2020-09-11 16:46:42 -0400

    signup page updates

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 
 class SignUp extends React.Component{
@@ -6,7 +8,19 @@ class SignUp extends React.Component{
     render(){
         return(
             <div>
-
+                <div>
+                    <h1 style={{}}>Welcome to Vivideo!</h1>
+                    <h2>Join our diverse users community.</h2>
+                </div>
+                <form>
+                    <TextField required id="standard-required" label="Username" /><br/>
+                    <TextField required id="standard-required" label="Email address" /><br/>
+                    <TextField required id="standard-required" label="Password" /><br/>
+                    <TextField required id="standard-required" label="Confirm password" /><br/><br/>
+                    <Button variant="contained" color="primary">
+                        Join our community!
+                    </Button><br/>
+                </form>
             </div>
         )
     }
