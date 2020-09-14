1da3ab7ef6a2f8e2a89e3816bdac6a32903b04b5
jerry <jerrylai19990120@gmail.com>
2020-09-14 16:23:48 -0400

    routes update

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component{
         <BrowserRouter>
           <Switch>
             <Route path="/home/room/:roomID" component={Room} />
-            <Route path='/home' component={()=>{return CreateRoom(useHistory())}}/>
+            <Route path='/home' component={()=>{if(!this.state.currentUser){useHistory().push('/')}else{return CreateRoom(useHistory())}}}/>
             <Route exact path={['/', '/home', '/home/room/:roomID']} component={()=>{if(this.state.currentUser){return CreateRoom(useHistory().push('/home'))}else{return <Login app={this}/>}}}/>
             <Route exact path="/signup" component={()=>{if(this.state.currentUser){return CreateRoom(useHistory().push('/home'))}else{return <SignUp app={this}/>}}}/>
           </Switch>
