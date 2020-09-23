b3a2d7f92aef28f4f3b5b76f1c11881967eb725b
jerry <jerrylai19990120@gmail.com>
2020-09-23 18:20:23 -0400

    login signup ui update

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import {login} from '../actions/userActions';
+import logo from '../images/logo.png';
 
 
 
@@ -17,23 +18,34 @@ class Login extends React.Component{
 
     render(){
         return(
-            <div style={{backgroundColor:'#F8C955', padding:"26vh", height:'343px'}}>
-                <h1 style={{color:'white'}}>Welcome to Vivideo!</h1>
-                <h2 style={{color:'white'}}>Join our diverse users community.</h2>
-                <form>
-                    <TextField id="standard-basic" label="Username" onChange={(event)=>{this.setState({username: event.target.value})}}/><br/>
-                    <TextField
-                        id="standard-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        onChange={(event)=>{this.setState({password: event.target.value})}}
-                    /><br/>
-                    <Button variant="contained" color="primary" style={{width:"16%", height:'4vh', marginTop:'2vh'}} onClick={()=>{login(this, this.props.app)}}>
-                        Log in
-                    </Button><br/><br/>
-                    <span style={{color:'#6F6F6F'}}>or <a href="/signup" style={{textDecoration:'none', color:'white'}}><strong>Sign up</strong></a> to join our community.</span>
-                </form>
+            <div style={{backgroundColor:'#8ED1CD', padding:"26vh", height:'343px'}}>
+                <div style={{height:"343px", width:'50%', float:'left'}}>
+                    <h1 style={{color:'white'}}>Welcome to Vivideo!</h1>
+                    <h2 style={{color:'white'}}>Join our diverse users community.</h2>
+                    <img src={logo}></img>
+                    <h2 style={{color:'white'}}>Enjoy the times with friends, family and loved ones.</h2>
+                </div>
+                <div style={{height:"343px", width:'50%', float:'left'}}>
+                    <div style={{height:"106%", width:"80%", backgroundColor:'white', paddingTop:'10%', borderRadius:'16px', marginLeft:'8%', marginTop:'-8%'}}>
+                        <h1 style={{color:'#67C7C1'}}>Log In</h1>
+                        <form>
+                            <TextField id="standard-basic" label="Username" onChange={(event)=>{this.setState({username: event.target.value})}}/><br/>
+                            <TextField
+                                id="standard-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                                onChange={(event)=>{this.setState({password: event.target.value})}}
+                            /><br/>
+                            <Button variant="contained" color="primary" style={{width:"28%", height:'4vh', marginTop:'2vh'}} onClick={()=>{login(this, this.props.app)}}>
+                                Log in
+                            </Button><br/><br/>
+                            <span style={{color:'#6F6F6F'}}>or <a href="/signup" style={{textDecoration:'none', color:'#67C7C1'}}><strong>Sign up</strong></a> to join our community.</span>
+                        </form>
+                    </div>
+                    
+                </div>
+                
             </div>
         )
     }
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import {signUp} from '../actions/userActions';
-
+import logo from '../images/logo.png';
 
 class SignUp extends React.Component{
 
@@ -19,21 +19,29 @@ class SignUp extends React.Component{
 
     render(){
         return(
-            <div style={{backgroundColor:'#F8C955', padding:"18vh", height:'457px'}}>
-                <div>
+            <div style={{backgroundColor:'#8ED1CD', padding:"18vh", height:'457px'}}>
+                <div style={{height:"343px", width:'50%', float:'left'}}>
                     <h1 style={{color:'white'}}>Welcome to Vivideo!</h1>
                     <h2 style={{color:'white'}}>Join our diverse users community.</h2>
+                    <img src={logo}></img>
+                    <h2 style={{color:'white'}}>Enjoy the times with friends, family and loved ones.</h2>
                 </div>
-                <form>
-                    <TextField required id="standard-required" label="Username" onChange={(event)=>{this.setState({username: event.target.value})}}/><br/>
-                    <TextField required id="standard-required" label="Email address" onChange={(event)=>{this.setState({email: event.target.value})}}/><br/>
-                    <TextField required id="standard-required" label="Password" type='password' onChange={(event)=>{this.setState({password: event.target.value})}}/><br/>
-                    <TextField required id="standard-required" label="Confirm password" type='password' onChange={(event)=>{this.setState({confirm: event.target.value})}}/><br/><br/>
-                    <Button variant="contained" color="primary" onClick={()=> {signUp(this, this.props.app)}}>
-                        Join our community!
-                    </Button><br/><br/>
-                    <span style={{color:'#6F6F6F'}}>Already have an account? <a href='/' style={{textDecoration:'none', color:'white'}}><strong>Log in</strong></a></span>
-                </form>
+                <div style={{height:"343px", width:'50%', float:'left'}}>
+                    <div style={{height:"128%", width:"80%", backgroundColor:'white', paddingTop:'10%', borderRadius:'16px', marginLeft:'8%', marginTop:'-8%'}}>
+                    <h1 style={{color:'#67C7C1'}}>Sign Up</h1>
+                        <form>
+                            <TextField required id="standard-required" label="Username" onChange={(event)=>{this.setState({username: event.target.value})}}/><br/>
+                            <TextField required id="standard-required" label="Email address" onChange={(event)=>{this.setState({email: event.target.value})}}/><br/>
+                            <TextField required id="standard-required" label="Password" type='password' onChange={(event)=>{this.setState({password: event.target.value})}}/><br/>
+                            <TextField required id="standard-required" label="Confirm password" type='password' onChange={(event)=>{this.setState({confirm: event.target.value})}}/><br/><br/>
+                            <Button variant="contained" color="primary" onClick={()=> {signUp(this, this.props.app)}}>
+                                Join our community!
+                            </Button><br/><br/>
+                            <span style={{color:'#6F6F6F'}}>Already have an account? <a href='/' style={{textDecoration:'none', color:'#67C7C1'}}><strong>Log in</strong></a></span>
+                        </form>
+                    </div>
+                </div>
+                
             </div>
         )
     }
